53ba778812b3ee99a02b93d8d0e6dc8b2ff7426f
James Grantham <james.r.grantham@gmail.com>
2020-04-23 17:03:21 +0100

    check collision on rotate

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,7 +1,7 @@
 import { useState, useCallback } from "react";
 
 import { TETROMINOS, randomTetromino } from "../tetrominos";
-import { STAGE_WIDTH } from "../gameHelpers";
+import { STAGE_WIDTH, checkCollision } from "../gameHelpers";
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
@@ -14,7 +14,7 @@ export const usePlayer = () => {
     const rotatedTetro = matrix.map((_, index) =>
       matrix.map((col) => col[index])
     );
-    if (direction > 0) return rotatedTetro.map(row => row.reverse());
+    if (direction > 0) return rotatedTetro.map((row) => row.reverse());
     return rotatedTetro.reverse();
 
     // mutates state
@@ -34,7 +34,18 @@ export const usePlayer = () => {
     const clonedPlayer = JSON.parse(JSON.stringify(player));
     clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
 
-    setPlayer(clonedPlayer);
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
+      setPlayer(clonedPlayer);
   };
 
   const updatePlayerPos = ({ x, y, collided }) => {
